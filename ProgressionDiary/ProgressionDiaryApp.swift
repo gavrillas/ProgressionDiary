c2a2a3d1a4ad2b89d5caf1ea28be8586f1172bbc
kristof <gavrillaskristof@gmail.com>
2022-03-14 14:36:36 +0100

    Add Firebase

--- ProgressionDiary/ProgressionDiaryApp.swift
+++ ProgressionDiary/ProgressionDiaryApp.swift
@@ -6,15 +6,18 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct ProgressionDiaryApp: App {
-    let persistenceController = PersistenceController.shared
+
+    init() {
+        FirebaseApp.configure()
+    }
 
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
